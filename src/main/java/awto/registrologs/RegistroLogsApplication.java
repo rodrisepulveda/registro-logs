ac17b42192f68f4dbeefca72c40f41743f1332e9
Rodrigo Sepúlveda Crisóstomo <rodrigo@rox.roxdom>
2020-09-08 22:20:03 -0300

    subir esqueleto del proyecto.

---
+++ src/main/java/awto/registrologs/RegistroLogsApplication.java
@@ -0,0 +1,13 @@
+package awto.registrologs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/awto/registrologs/RegistroLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package awto.registrologs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
